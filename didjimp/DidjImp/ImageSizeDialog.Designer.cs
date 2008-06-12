fe2b601577c099c5078b39d1569506ee2d9cfe88
JesusFreke <jesusfreke@jesusfreke.com>
2008-06-12 07:40:08 +0000

    Updated the tab order on the "Choose Image Size" dialog
    git-svn-id: svn://svn.code.sf.net/p/didjimp/code/Trunk@44 6ca9c07c-e44e-0410-bd48-f491998bae60

--- didjimp/DidjImp/ImageSizeDialog.Designer.cs
+++ didjimp/DidjImp/ImageSizeDialog.Designer.cs
@@ -146,7 +146,7 @@ namespace DidjImp
             0});
 			this.numHeight.Name = "numHeight";
 			this.numHeight.Size = new System.Drawing.Size(62, 20);
-			this.numHeight.TabIndex = 0;
+			this.numHeight.TabIndex = 1;
 			this.numHeight.Value = new decimal(new int[] {
             1,
             0,
@@ -159,7 +159,7 @@ namespace DidjImp
 			this.btnOK.Location = new System.Drawing.Point(128, 95);
 			this.btnOK.Name = "btnOK";
 			this.btnOK.Size = new System.Drawing.Size(75, 23);
-			this.btnOK.TabIndex = 5;
+			this.btnOK.TabIndex = 2;
 			this.btnOK.Text = "OK";
 			this.btnOK.UseVisualStyleBackColor = true;
 			// 
@@ -169,7 +169,7 @@ namespace DidjImp
 			this.btnCancel.Location = new System.Drawing.Point(43, 95);
 			this.btnCancel.Name = "btnCancel";
 			this.btnCancel.Size = new System.Drawing.Size(75, 23);
-			this.btnCancel.TabIndex = 6;
+			this.btnCancel.TabIndex = 3;
 			this.btnCancel.Text = "Cancel";
 			this.btnCancel.UseVisualStyleBackColor = true;
 			// 
